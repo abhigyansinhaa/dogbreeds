a0ff985e61e99ec0965f1ea4062722b50fb320ca
abhigyansinhaa <140688491+abhigyansinhaa@users.noreply.github.com>
2024-02-02 22:03:44 +0530

    Update dogbreeds.py

--- dogbreeds.py
+++ dogbreeds.py
@@ -3,5 +3,21 @@
 import numpy as np
 import cv2
 
-IMAGE_SIZE=(331,331)
-IMAGE_FULL_SIZE=(331,331,3)
+IMAGE_SIZE=(221,221)
+IMAGE_FULL_SIZE=(221,221,4)
+
+trainMyImageFolder ="C:\Users\KIIT0001\Downloads\dog-breed-identification.zip\train"
+
+#Now csv file is to be loaded
+import pandas as pd
+read=pd.read_csv('C:\Users\KIIT0001\Downloads\dog-breed-identification.zip\labels.csv')
+
+print("head of labels:")
+print("===============")
+
+print(df.head())
+print(df.describe())
+
+print("Group by labels:")
+grouplabels=df.groupby("breed")["id"].count()
+print(grouplabels.head(10))
